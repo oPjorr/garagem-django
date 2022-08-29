f8c7fa2c9cad0611516c8b7a02195225bca34e41
Paulo <paulorolinski.ifc@gmail.com>
2022-08-28 23:49:44 -0300

    registrando as rotas

--- garagem/urls.py
+++ garagem/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from core.views import CategoriaViewSet, MarcaViewSet
+
+router = DefaultRouter()
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"marcas", MarcaViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
