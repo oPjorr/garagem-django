b5045adffdd79aedf1bbc9d04aae43224fbe7853
Paulo <paulorolinski.ifc@gmail.com>
2022-08-28 23:21:09 -0300

    criando views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from core.models import Categoria, Marca, Carro
+from core.serializers import (
+    CategoriaSerializer,
+    MarcaSerializer,
+    CarroSerializer,
+    CarroDetailSerializer,
+)
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class CarroViewSet(ModelViewSet):
+    queryset = Carro.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return CarroDetailSerializer
+        return CarroSerializer
