e065861035fa6f16c90a62c9dec1664c8a6f0bee
Paulo <paulorolinski.ifc@gmail.com>
2022-08-28 22:52:29 -0300

    criando models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
